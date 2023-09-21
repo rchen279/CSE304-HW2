f0eb406c6757d12ec1bcb823d9ef46b0369649c8
rchen279 <ryan.chen.1@stonybrook.edu>
2023-09-21 00:37:14 -0400

    added token specs for scanner, newline, ignore, error, and single/multiline comments

--- decaf_lexer.py
+++ decaf_lexer.py
@@ -3,7 +3,6 @@
 #
 #
 # -------------------------------------------------------------------
-
 # reserved keywords
 reserved = {
     "boolean" : "BOOLEAN",
@@ -37,6 +36,7 @@ tokens = ["ID", # identifier
           "INT_CONST", #constants
           "FLOAT_CONST",
           "STRING_CONST",
+
           # syntax stuff and operators
           "L_CURLY_BRACE", # {
           "R_CURLY_BRACE", # }
@@ -58,25 +58,128 @@ tokens = ["ID", # identifier
           "L_THAN_EQUAL_TO_OP", # <=
           "NEG_OP", # ! negation
           "LOGICAL_AND_OP", # && AND
-          "LOGICAL_OR_OP", # || operator
+          "LOGICAL_OR_OP" # || operator
           ]
 
 tokens = tokens + list(reserved.values())
 
-# token specifications
+# regular expression rules for simple tokens (not sure if this is right)
 
+# longer expressions come first
+t_STRING_CONST = r"\"(\w|\s)+\""
+t_L_CURLY_BRACE = r"\{"
+t_R_CURLY_BRACE = r"\}"
+t_L_PAREN = r"\("
+t_R_PAREN = r"\)"
+t_SEMI_COLON = r";"
+t_POST_INCR_OP = r"\+\+"
+t_POST_DECR_OP = r"--"
+t_G_THAN_EQUAL_TO_OP = r">="
+t_L_THAN_EQUAL_TO_OP = r"<="
+t_EQUALITY_OP = r"=="
+t_DISEQUALITY_OP = r"!="
+t_LOGICAL_AND_OP = r"&&"
+t_LOGICAL_OR_OP = r"\|\|"
+
+# shorter expression come later
+t_ASSIGNMENT_OP = r"="
+t_PLUS = r"\+"
+t_MINUS = r"-"
+t_TIMES = r"\*"
+t_DIVIDE = r"/"
+t_L_THAN_OP = r"<"
+t_G_THAN_OP = r">"
+t_NEG_OP = r"!"
+
+
+# regular expression rule with action code
 def t_IDENTIFIER(t):
     r'[a-zA-Z][a-zA-Z0-9_]*'
     t.type = reserved.get(t.value, "ID") # check for reserved keywords
     return t
 
+# float constants have a decimal point and a sequence of 1+ digits on either side
+def t_FLOAT_CONST(t):
+    r'\d+\.\d+'
+    t.value = float(t.value)
+    return t
+
+# Integer constants are one or more digits [0-9]
+def t_INT_CONST(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+# define rule to track line no.
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+# A string containing ignored characters
+t_ignore = " \t\n"
+
+# scanning errors
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+# ignoring comments
+
+# handle multi-line comments
+
+def t_COMMENT_MULTI_LINE(t):
+    r"\/\*[\s\S]*?\*\/"
+    pass
+
+# handle single-line comments
+def t_COMMENT_SINGLE_LINE(t):
+    r"\/\/[^\n]*"
+    pass
+
 # build the lexer for testing purposes (delete later please)
 
 import ply.lex as lex
-# lexer = lex.lex()
+lexer = lex.lex()
 
-# lexer.input("test.txt")
-# next_token = lexer.token()
-# while next_token != None:
-#     print(next_token)
-#     next_token = lexer.token()
+lexer.input(
+    """
+    int main() {
+        /* This is a multi-line comment */
+        int a = 10; // This is a single-line comment
+
+        // Another single-line comment
+        for (int i = 0; i < 5; i++) {
+            a++; // Increment a
+        }
+
+        /* A
+        multi-line
+        comment
+        */
+    return 0;
+}
+    """
+    # '''
+    # class rfib{
+    #     static int fib(int n) {
+    #         if (n <= 2)
+    #             return 1;
+    #         else
+    #             return fib(n-1) + fib(n-2);
+
+    #     }
+    #     public static void main() {
+    #         int n;
+    #         n = In.scan_int();
+    #         Out.print("Fib = ");
+    #         Out.print(fib(n));
+    #         Out.print("\n");
+    #         }
+    #     }
+    # '''
+)
+next_token = lexer.token()
+while next_token != None:
+    print(next_token)
+    next_token = lexer.token()
+
