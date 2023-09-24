cbc1877675aeebc392c0983806d081765ca69d2c
rchen279 <ryan.chen.1@stonybrook.edu>
2023-09-24 01:05:31 -0400

    some additions to parser

--- decaf_parser.py
+++ decaf_parser.py
@@ -2,17 +2,40 @@
 from decaf_lexer import tokens
 
 # define grammar rules
-
-# starting rule????
-# def p_program(p):
-#     '''
-#     program : class_decl*
-#     '''
-
+# starting rule -- program
 def p_program(p):
     '''
-    program : ID
-            | ID program
+    program : class_decl program
+            | empty
+    '''
+
+# empty production
+def p_empty(p):
+    'empty :'
+    pass
+
+def p_class_declaration(p):
+    '''
+    class_decl : CLASS id optionalExtendsId L_CURLY_BRACE class_body_decl_plus R_CURLY_BRACE
+    '''
+
+def p_optionalExtendsId(p):
+    '''
+    optionalExtendsId : EXTENDS id
+                | empty
+    '''
+
+def p_classBodyDeclPlus(p):
+    '''
+    class_body_decl_plus : class_body_decl
+                        | class_body_decl class_body_decl_plus
+    '''
+
+def p_class_body_decl(p):
+    '''
+    class_body_decl : field decl
+                    | method_decl
+                    | constructor_decl
     '''
 
 
